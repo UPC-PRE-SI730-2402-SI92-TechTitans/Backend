c19e520a9cb2bf6e45cfb108673fb15fb042eace
melinaasantillan <129870878+melinaasantillan@users.noreply.github.com>
2024-11-02 19:00:09 -0500

    feat: add Contact

---
+++ Application/Contact/QueryServices/ContactQueryService.cs
@@ -0,0 +1,27 @@
+using Domain.Contact.Model.Entities;
+using Domain.Contact.Model.Queries;
+using Domain.Contact.Repositories;
+using Domain.Contact.Services;
+
+namespace Application.Contact.QueryServices;
+
+public class ContactQueryService : IContactQueryService
+{
+    private readonly IContactRepository _repository;
+
+    public ContactQueryService(IContactRepository repository)
+    {
+        _repository = repository;
+    }
+
+    //overload
+    public async Task<IEnumerable<Contacto>?> Handle(GetAllContactsQuery query)
+    {
+        return await _repository.ListAsync();
+    }
+
+    public async Task<Contacto?> Handle(GetContactByIdQuery query)
+    {
+        return await _repository.FindByIdAsync(query.Id);
+    }
+}
