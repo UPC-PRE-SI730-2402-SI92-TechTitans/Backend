18d45d344db0299da9ec18d68a10731f90e8509c
melinaasantillan <129870878+melinaasantillan@users.noreply.github.com>
2024-11-18 16:35:58 -0500

    feat: update Program.cs

--- Presentation/Program.cs
+++ Presentation/Program.cs
@@ -1,86 +1,75 @@
-using Microsoft.EntityFrameworkCore;
-using Infrastructure.Groups.Persistence;
-using Infrastructure.Groups.Repositories;
+using Application.FinanceGuard.CommandServices;
+using Application.FinanceGuard.QueryServices;
+using Domain.FinanceGuard.Repositories;
+using Domain.FinanceGuard.Services;
 using Domain.Groups.Repositories;
-using Microsoft.OpenApi.Models;
-using System;
+using Domain.Shared;
+using Infrastructure.FinanceGuard;
+using Infrastructure.Groups.Repositories;
+using Infrastructure.Shared.Persistence.EFC.Configuration;
+using Infrastructure.Shared.Persistence.EFC.Repositories;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
-var connectionString = builder.Configuration.GetConnectionString("financeGuardConnection");
+// Add services to the container.
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
 
-// Registro de GroupDbContext
-builder.Services.AddDbContext<GroupDbContext>(options =>
-{
-    if (!string.IsNullOrEmpty(connectionString))
-    {
-        if (builder.Environment.IsDevelopment())
-        {
-            options.UseMySQL(connectionString)
-                .LogTo(Console.WriteLine, LogLevel.Information)
-                .EnableSensitiveDataLogging()
-                .EnableDetailedErrors();
-        }
-        else if (builder.Environment.IsProduction())
-        {
-            options.UseMySQL(connectionString)
-                .LogTo(Console.WriteLine, LogLevel.Error)
-                .EnableDetailedErrors();
-        }
-    }
-});
-
-// Registro de repositorios y servicios para Groups
+builder.Services.AddScoped<IContactRepository, ContactRepository>();
+builder.Services.AddScoped<IContactQueryService, ContactQueryService>();
+builder.Services.AddScoped<IContactCommandService, ContactCommandService>();
 builder.Services.AddScoped<IGroupRepository, GroupRepository>();
 builder.Services.AddScoped<Application.Groups.CommandServices.GroupCommandService>();
 builder.Services.AddScoped<Application.Groups.QueryServices.GroupQueryService>();
+builder.Services.AddScoped<IUnitOfWork, UnitOfWork>();
 
-// Configuración de CORS
-builder.Services.AddCors(options =>
-{
-    options.AddPolicy("CorsPolicy", policy =>
+//Conexión a MySql
+var connectionString = builder.Configuration.GetConnectionString("financeGuardConnection");
+
+builder.Services.AddDbContext<AppDbContext>(
+    options =>
     {
-        policy.AllowAnyOrigin()
-            .AllowAnyMethod()
-            .AllowAnyHeader();
+        if (connectionString != null)
+            if (builder.Environment.IsDevelopment())
+                options.UseMySQL(connectionString)
+                    .LogTo(Console.WriteLine, LogLevel.Information)
+                    .EnableSensitiveDataLogging()
+                    .EnableDetailedErrors();
+            else if (builder.Environment.IsProduction())
+                options.UseMySQL(connectionString)
+                    .LogTo(Console.WriteLine, LogLevel.Error)
+                    .EnableDetailedErrors();
     });
-});
-
-// Configuración de JSON
-builder.Services.AddControllers().AddJsonOptions(options =>
-{
-    options.JsonSerializerOptions.PropertyNamingPolicy = System.Text.Json.JsonNamingPolicy.CamelCase;
-    options.JsonSerializerOptions.DefaultIgnoreCondition = System.Text.Json.Serialization.JsonIgnoreCondition.WhenWritingNull;
-    options.JsonSerializerOptions.ReferenceHandler = System.Text.Json.Serialization.ReferenceHandler.IgnoreCycles;
-});
-
-// Configuración de Swagger
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen(c =>
-{
-    c.SwaggerDoc("v1", new OpenApiInfo { Title = "Finance guard API", Version = "v1" });
-});
 
 var app = builder.Build();
 
-// Asegurar la creación de la base de datos para ambos contextos
-using (var scope = app.Services.CreateScope())
-{
-    var groupDbContext = scope.ServiceProvider.GetRequiredService<GroupDbContext>();
+EnsureDataBaseCreation(app);
 
-    groupDbContext.Database.EnsureCreated();
-}
-
-// Configuración de Swagger
-if (app.Environment.IsDevelopment())
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment() || app.Environment.IsProduction())
 {
     app.UseSwagger();
-    app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "Finance Guard API v1"));
+    app.UseSwaggerUI();
 }
 
+app.UseCors(x => x
+    .SetIsOriginAllowed(origin => true)
+    .AllowAnyMethod()
+    .AllowAnyHeader()
+    .AllowCredentials());
+
 app.UseHttpsRedirection();
-app.UseCors("CorsPolicy");
-app.UseAuthorization();
-
 app.MapControllers();
-
 app.Run();
+
+void EnsureDataBaseCreation(WebApplication app)
+{
+    using (var scope = app.Services.CreateScope())
+    {
+        var context = scope.ServiceProvider.GetRequiredService<AppDbContext>();
+        context.Database.EnsureCreated();
+    }
+}
