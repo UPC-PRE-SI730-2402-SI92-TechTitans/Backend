92a3ee061f829ed3560b23041c8d38b38417aa8f
melinaasantillan <129870878+melinaasantillan@users.noreply.github.com>
2024-11-18 16:41:43 -0500

    feat: update 20241116223903_InitialCreate.cs

--- Infrastructure/GroupsMigrations/20241116223903_InitialCreate.cs
+++ Infrastructure/GroupsMigrations/20241116223903_InitialCreate.cs
@@ -3,7 +3,7 @@ using Microsoft.EntityFrameworkCore.Migrations;
 
 #nullable disable
 
-namespace Infrastructure.GroupsMigrations
+namespace Infrastructure.Migrations
 {
     /// <inheritdoc />
     public partial class InitialCreate : Migration
